0495fab8cb865c4c5264d225739c1b4a2ccd61c0
Jonathan Leung <jonlhk@gmail.com>
2013-04-07 16:16:53 -0400

    App loading using ajax

--- static/js/index.js
+++ static/js/index.js
@@ -3,24 +3,57 @@ $(document).ready(function(){
  });
 
  function login() {
- 	//login pressed
+ 	//Handle login button pressed
 
- 	//Initiate login
+ 	//Get input values and checks if okay
+ 	var usr = $("#login_username").val();
+ 	var pwd = $("#login_password").val();
+
+ 	console.log(usr + ": " + pwd);
 
  	//If okay load menu
  	var content_area = $("#content_area");
  	content_area.empty();
  	var menu = $("<ul>");
- 	menu.append($("<li>").html("Play").attr("id",play_button));
- 	menu.append($("<li>").html("Profile"));
- 	menu.append($("<li>").html("Settings"));
+ 	menu.append($("<li>").html("Play").attr("id","play_button"));
+ 	menu.append($("<li>").html("Profile").attr("id","profile_button"));
+ 	menu.append($("<li>").html("Settings").attr("id","settings_button"));
+
+ 	//Need to add logout/back button
 
  	content_area.append(menu);
 
-
-
+ 	//Add touch listeners
+ 	$("#play_button").onButtonTap(loadCanvas);
+ 	$("#profile_button").onButtonTap(loadProfile);
+ 	$("#settings_button").onButtonTap(loadSettings);
  }
 
  function register() {
- 	window.open("menu.html","_self");
+ 	//Handle register button pressed
+ }
+
+ function loadCanvas() {
+ 	//Load the canvas
+ 	$("body").html("Loaded!");
+
+	canvasWidth = $(window).width();
+	canvasHeight = $(window).height();
+
+	var c = $("<canvas>").attr({id:"myCanvas",width:canvasWidth,height:canvasHeight}).html("Cannot Load!");
+
+	$("body").html(c);
+
+	window.canvas = document.getElementById("myCanvas");
+	window.ctx = canvas.getContext("2d");
+
+	//draw();
+ }
+
+ function loadProfile() {
+ 	$("#content_area").html("profile");
+ }
+
+ function loadSettings() {
+ 	$("#content_area").html("settings");
  }
