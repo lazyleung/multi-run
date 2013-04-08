ddc72045dada1fb219120dbdb8561fc9f3556581
Alvin Wang <alvin7@gmail.com>
2013-04-08 06:04:55 -0400

    added touch gestuers and main player

--- static/js/game.js
+++ static/js/game.js
@@ -1,12 +1,15 @@
 //Highlevel control of game
+var player;
+var gameInterval = 25;
 
 function initGame() {
 	//Sets up the game
 	window.canvas = document.getElementById("myCanvas");
 	window.ctx = canvas.getContext("2d");
 	
+	// Create update interval
 	//Create new player
-	player = new Player(100, 100);
+	player = new Player(750, 500);
 	//Add control handlers
 
 	draw();
@@ -15,14 +18,22 @@ function initGame() {
 	startGame();
 }
 
-function startGame() {
-
-}
-
 function draw() {
-	// Draw player
-	player.draw(ctx);
+
 	//Draw all elements
 	ctx.fillStyle = "rgba(0,0,0,0.9)";
 	ctx.fillRect(canvasWidth/2 - 300, canvasHeight/2 - 300, 600, 600);
+
+	// Draw player
+	player.draw(ctx);
+}
+
+function update() {
+	player.update();
+	draw();
+}
+
+function startGame() {
+	var gameInterval = setInterval(update, gameInterval);
+
 }
--- static/js/player.js
+++ static/js/player.js
@@ -1,14 +1,72 @@
 function Player(playerX, playerY) {
+	this.x = playerX;
+	this.y = playerY;
+	this.airFriction = .1;
+	this.gravity = .1;
+	this.onFloor = true;
+	this.width = 50;
+	this.height = 50;
+	this.speed = {x : 0, y : 0};
+	this.image = new Image();
+	this.image.src = "/images/wooden_crate.png";
 
 	this.init = function() {
-		this.x = playerX;
-		this.y = playerY;
-		this.speed = (0,0);
-		this.image = new Image()
-		this.image.src = "../../Art\ assets/wooden_crate.png"
+		// Setup touch handler
+		$('body').hammer().on("swipeup", function(event) {
+			event.gesture.preventDefault();
+		    console.log(this, event);
+		    if (Math.abs(event.gesture.deltaY) <= 300) {
+		    	console.log("low jump");
+		    	lowJump();
+		    }
+		    else {
+		    	console.log("high jump");
+		    	highJump();
+		    }
+		});
+		$('body').hammer().on("swipedown", function(event) {
+			event.gesture.preventDefault();
+		    console.log("swipedown");
+		});
+		$('body').hammer().on("swipeleft", function(event) {
+			event.gesture.preventDefault();
+		    console.log("swipeleft");
+		});
 	}
 
 	this.draw = function(ctx) {
-		ctx.drawImage(this.image, 281, 5, 14, 14, this.x, this.y, 25, 25);
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 	}
+
+	var lowJump = function(ctx) {
+		this.speed.y = -75;
+	}.bind(this);
+
+	var highJump = function(ctx) {
+		this.speed.y = -150;
+	}.bind(this);
+
+	var slowDown = function(ctx) {
+
+	}.bind(this);
+
+	this.update = function() {
+		// Check on floor
+		if (this.y < 550)
+			this.onFloor = false;
+		else
+			this.onFloor = true;
+
+		this.y += this.speed.y;
+		this.x += this.speed.x;
+		if (Math.abs(this.speed.y) < this.airFriction)
+			this.speed.y = 0;
+		else if (this.speed.y < 0)
+			this.speed.y += this.airFriction;
+		if (!this.onFloor && this.speed.y >= 0)
+			this.speed.y += this.gravity;
+		else
+			this.speed.y = 0
+	}
+	this.init();
 }
