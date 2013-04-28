55044d18be753fd980e2c47bd3ac52c493a470aa
Alvin Wang <alvin7@gmail.com>
2013-04-28 01:32:12 -0400

    added animation

--- static/js/player.js
+++ static/js/player.js
@@ -13,7 +13,8 @@ function Player(playerX, playerY) {
 	this.jumpTimeLeft = 0;
 	this.floor = canvasHeight - window.block_y;
 	this.xSpeedLimit = 25;
-	this.image.src = "/images/Dinosaur.png";
+	this.animationFrame = 0;
+	this.image.src = "/images/dinosaur_animation.png";
 
 	this.init = function() {
 
@@ -48,7 +49,7 @@ function Player(playerX, playerY) {
 	}
 
 	this.draw = function() {
-		ctx.drawImage(this.image, this.x, this.y - this.height, this.width, this.height);
+		ctx.drawImage(this.image, ( 247 * Math.ceil(this.animationFrame)), 0, 247, 475, this.x, this.y - this.height, this.width, this.height);
 	}
 
 	var lowJump = function() {
@@ -68,6 +69,9 @@ function Player(playerX, playerY) {
 	}.bind(this);
 
 	this.update = function(terrain) {
+
+		// advance the animation frame
+		this.animationFrame = (this.animationFrame + (this.speed.x / 40)) % 5
 		progress = Math.floor((this.x + this.width)/window.block_x);
 
 		var y_block = Math.ceil(this.y/window.block_y);
